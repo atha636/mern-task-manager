4e98eda62e2dce820bfd702907a0d13f8cb58b75
Atharv Patidar <atharvpatidar2005@gmail.com>
2026-05-24 22:35:44 +0530

    add the prod url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const API = axios.create({
 
-    baseURL:"http://localhost:5000/api"
+    baseURL:"https://mern-task-manager-production-0e30.up.railway.app/api"
+    
 });
 
 API.interceptors.request.use(
